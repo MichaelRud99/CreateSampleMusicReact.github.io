c6bf7a0d22502bccc68e42387abbb677ccb98b67
Michael <monsegard@gmail.com>
2022-09-06 15:04:58 +0300

    Change searchLetters 27

--- src/utils/searchLetters/searchLetters.js
+++ src/utils/searchLetters/searchLetters.js
@@ -1,17 +1,37 @@
+const SearchAccordance = (searchValue, letters, letterSearch, count) => {
+   for (let j = 0; j < searchValue.length; j++) {
+      if (letters[j] === letterSearch[j]) {
+         count++;
+      } else {
+         break;
+      }
+   }
+   return count;
+};
+
 const searchLetters = (searchValue, value) => {
    let foundArr = [];
    const letterSearch = searchValue.split("");
    let count = 0;
+   let ij = 0;
    for (let i = 0; i < value.length; i++) {
-      let letters = value[i].toLowerCase().split("");
+      let words = value[i].toLowerCase().split(" ");
+      if (words.length > 1) {
+         while (true) {
+            if (words.length <= ij) {
+               break;
+            }
+            let letters = words[ij].toLowerCase().split("");
+            count = SearchAccordance(searchValue, letters, letterSearch, count);
 
-      for (let j = 0; j < searchValue.length; j++) {
-         if (letters[j] === letterSearch[j]) {
-            count++;
-         } else {
-            break;
+            ij++;
          }
+         ij = 0;
+      } else {
+         let letters = words[0].toLowerCase().split("");
+         count = SearchAccordance(searchValue, letters, letterSearch, count);
       }
+
       if (count === searchValue.length) {
          foundArr.push(value[i]);
       }
