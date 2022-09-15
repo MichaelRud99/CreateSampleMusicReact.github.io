a9f4a6b7673b32b55fc6208d00bb8cf48763126b
Michael <monsegard@gmail.com>
2022-09-15 18:29:41 +0300

    Change useAction 36

--- src/Components/Hooks/useActotion.jsx
+++ src/Components/Hooks/useActotion.jsx
@@ -1,11 +1,17 @@
 import { bindActionCreators } from "redux";
 import { useDispatch } from "react-redux";
+import { useMemo } from "react";
 
-export function useActions(actions) {
+export function useActions(actions, deps) {
    const dispatch = useDispatch();
-
-   if (Array.isArray(actions)) {
-      return actions.map((a) => bindActionCreators(a, dispatch));
-   }
-   return bindActionCreators(actions, dispatch);
+   return useMemo(
+      (payload) => {
+         payload = {};
+         if (Array.isArray(actions)) {
+            return actions.map((a) => bindActionCreators(a, dispatch, payload));
+         }
+         return bindActionCreators(actions, dispatch, payload);
+      },
+      deps ? [dispatch, ...deps] : [dispatch]
+   );
 }
